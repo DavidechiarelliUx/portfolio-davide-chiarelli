db5f95457a1ed1db787dd595674c56791e230e3f
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-11-07 10:07:51 +0100

    Set up SASS structure and global styles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    additionalData: `
+    @import "./src/styles/scss/all.scss";
+    `,
+  },
 }
 
 module.exports = nextConfig
