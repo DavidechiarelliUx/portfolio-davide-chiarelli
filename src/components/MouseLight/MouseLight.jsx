2dec2725617fafb74f29a6da9c26c72a06b43903
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-11-21 17:03:45 +0100

    feat: add header section,logo and hamburger menu

--- src/components/MouseLight/MouseLight.jsx
+++ src/components/MouseLight/MouseLight.jsx
@@ -1,29 +1,40 @@
 import React, { useEffect } from 'react';
 import styles from './MouseLight.module.scss';
 
+// Definizione della funzione isMobile
+const isMobile = () => (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || 
+  'ontouchstart' in document.documentElement
+);
 
 const MouseLight = () => {
   useEffect(() => {
-    const updateMouseLightPosition = (e) => {
-      const light = document.getElementById('mouse-light');
-      light.style.top = `${e.pageY}px`;
-      light.style.left = `${e.pageX}px`;
-      light.style.opacity = 1;
-    };
+    // Utilizzo della funzione isMobile
+    if (!isMobile()) {
+      const updateMouseLightPosition = (e) => {
+        const light = document.getElementById('mouse-light');
+        if (light) {
+          light.style.top = `${e.pageY}px`;
+          light.style.left = `${e.pageX}px`;
+          light.style.opacity = 1;
+        }
+      };
 
-    document.addEventListener('mousemove', updateMouseLightPosition);
-    document.addEventListener('mouseout', () => {
-      const light = document.getElementById('mouse-light');
-      light.style.opacity = 0;
-    });
+      document.addEventListener('mousemove', updateMouseLightPosition);
+      document.addEventListener('mouseout', () => {
+        const light = document.getElementById('mouse-light');
+        if (light) {
+          light.style.opacity = 0;
+        }
+      });
 
-    return () => {
-      document.removeEventListener('mousemove', updateMouseLightPosition);
-    };
+      return () => {
+        document.removeEventListener('mousemove', updateMouseLightPosition);
+      };
+    }
   }, []);
 
-  return <div id="mouse-light" className={styles.mouseLight}>
-  </div>;
+  return <div id="mouse-light" className={styles.mouseLight}></div>;
 };
 
 export default MouseLight;
